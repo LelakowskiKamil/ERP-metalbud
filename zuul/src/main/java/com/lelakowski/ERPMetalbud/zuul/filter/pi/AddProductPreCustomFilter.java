47ad3778a09385d3492096c96213f6521ae1799f
Kamil Lelakowski <kamilel1997@gmail.com>
2022-01-08 10:21:22 +0100

    #20 Add Zuul api gateway
    *Added Zuul module
    *Connected each api of the modules with the zuul
    *Fixed bug in ColorService implementation

---
+++ zuul/src/main/java/com/lelakowski/ERPMetalbud/zuul/filter/pi/AddProductPreCustomFilter.java
@@ -0,0 +1,50 @@
+package com.lelakowski.ERPMetalbud.zuul.filter.pi;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletInputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+@Component
+@Slf4j
+public class AddProductPreCustomFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext context = RequestContext.getCurrentContext();
+        return context.getRequest().getRequestURI().startsWith("/api/v1/pi/products");
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        String method = requestContext.getRequest().getMethod();
+        String contentType = requestContext.getRequest().getContentType();
+        String body = null;
+        try {
+            ServletInputStream inputStream = requestContext.getRequest().getInputStream();
+            body = StreamUtils.copyToString(inputStream, Charset.forName("UTF-8"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        log.info("Method: " + method + ". Content type: " + contentType + ". Body: \n" + body);
+        return null;
+    }
+}
